e05e09ebcfce83dc88f4519e77b1d3b39e8c56b3
kangtongyong <kangtongyong54@gmail.com>
2025-11-19 13:37:51 +0900

    25-11-19 main.h 추가

--- Project04/main.h
+++ Project04/main.h
@@ -1,1 +1,24 @@
+// 대충 짠 헤더
+
+//#ifndef <stdio.h>
+//#define <stdio.h>
+//#include <stdio.h>
+//#endif;
+
+
+
 #pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+// 귀찮아서 선언
+#include <stdio.h>
+#include <string.h>
+
+
+// File04.c 함수 선언
+void file04(void);
+int maxresult(int m, int n);
+void leastresult(int M, int N, int RESULT);
